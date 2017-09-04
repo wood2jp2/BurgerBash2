e51b1638e86489f237014a1d2d59011620bd4627
Josh Wood <wood2jp2@gmail.com>
2017-09-04 18:20:35 -0400

    fixed something

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
+var connection;
 
 // requiring connection to sql DB
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
